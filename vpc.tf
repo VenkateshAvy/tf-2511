fc7f8da734aa03d72c521f6f1993e652ea737184
venkateshAvy <venkateshavy123@gmail.com>
2024-04-24 15:49:04 +0530

    attached igw

--- vpc.tf
+++ vpc.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "ibm-web-rtb" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.ibm-igw.id
   }
 
   tags = {
