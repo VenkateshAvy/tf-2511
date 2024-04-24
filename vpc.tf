8800d6242822636dfafd6da826072e36d5c79fda
venkateshAvy <venkateshavy123@gmail.com>
2024-04-24 15:39:38 +0530

    created web and db subnets

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,19 @@ resource "aws_vpc" "ibm-vpc" {
     Name = "ibm-vpc"
   }
 }
+resource "aws_subnet" "ibm-web-subnet" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.0.0/20"
+
+  tags = {
+    Name = "ibm-web-subnet"
+  }
+}
+resource "aws_subnet" "ibm-db-subnet" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.16.0/20"
+
+  tags = {
+    Name = "ibm-db-subnet"
+  }
+}
